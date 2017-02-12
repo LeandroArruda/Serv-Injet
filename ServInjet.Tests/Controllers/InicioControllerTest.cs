063b833bf191ca388d1fecbcb2f57cdc6d810c98
LeandroArruda <leandro.arruda@outlook.com>
2017-02-12 14:59:59 -0200

    Initial commit

---
+++ ServInjet.Tests/Controllers/InicioControllerTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ServInjet;
+using ServInjet.Controllers;
+
+namespace ServInjet.Tests.Controllers
+{
+    [TestClass]
+    public class InicioControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            InicioController controller = new InicioController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.AreEqual("Modify this template to jump-start your ASP.NET MVC application.", result.ViewBag.Message);
+        }
+
+        [TestMethod]
+        public void Sobre()
+        {
+            // Arrange
+            InicioController controller = new InicioController();
+
+            // Act
+            ViewResult result = controller.Sobre() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void Contato()
+        {
+            // Arrange
+            InicioController controller = new InicioController();
+
+            // Act
+            ViewResult result = controller.Contato() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ ServInjet/Controllers/InicioController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ServInjet.Controllers
+{
+    public class InicioController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Sobre()
+        {
+            return View();
+        }
+
+        public ActionResult Contato()
+        {
+            return View();
+        }
+
+        public ActionResult Projetos()
+        {
+            return View();
+        }
+
+        public ActionResult Servico()
+        {
+            return View();
+        }
+    }
+}
