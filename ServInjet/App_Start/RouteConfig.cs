9974bc95bad113f17c2212979d17e08136cdee13
LeandroArruda <leandro.arruda@outlook.com>
2017-02-12 15:27:56 -0200

    Criacao da Area de Admin, estado inicial

--- ServInjet/App_Start/RouteConfig.cs
+++ ServInjet/App_Start/RouteConfig.cs
@@ -16,7 +16,8 @@ namespace ServInjet
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Inicio", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Inicio", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "ServInjet.Controllers" }
             );
         }
     }
